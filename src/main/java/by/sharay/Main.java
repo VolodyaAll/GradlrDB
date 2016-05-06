b4383694377cf727d2afaa0fd17f1d0434d372a4
VolodyaAll <volodya22all@gmail.com>
2016-05-06 18:29:30 +0300

    Firs commit

---
+++ src/main/java/by/sharay/Main.java
@@ -0,0 +1,10 @@
+package by.sharay;
+/**
+ * Created by All on 03.05.2016.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
